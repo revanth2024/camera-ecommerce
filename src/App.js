5d07e1fdbd57e578a95142c01f406f9b38886a86
revanth2024 <revanth2899@gmail.com>
2024-06-25 12:51:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import "./App.css";
-import Header from "./Header";
-import Product from "./Product";
-import Home from "./Home";
-import About from "./About";
-import Cart from "./Cart";
+import { Header } from "./Header";
+import { Product } from "./Product";
+import { Home } from "./Home";
+import { About } from "./About";
+import { Cart } from "./Cart";
 import Footer from "./Footer";
 
 function App() {
   useEffect(() => {
-    AOS.init({ duration: 1000 });
+    AOS.init({
+      duration: 1000,
+    });
   }, []);
 
   const [cart, setCart] = useState([]);
