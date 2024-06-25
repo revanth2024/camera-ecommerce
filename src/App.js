65da77f04fda52cd0248b954431b82533b31ed34
revanth2024 <revanth2899@gmail.com>
2024-06-25 12:22:30 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./App.css";
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header } from "./Header";
 import { Product } from "./Product";
 import { Home } from "./Home";
 import { About } from "./About";
 import { Cart } from "./Cart";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import Footer from "./Footer";
 
 function App() {
-  React.useEffect(() => {
+  useEffect(() => {
     AOS.init({
       duration: 1000,
     });
   }, []);
 
-  const [cart, setCart] = React.useState([]);
+  const [cart, setCart] = useState([]);
 
   return (
     <div className="App">
       <BrowserRouter>
+        <Header cart={cart} />
         <Routes>
-          <Route path="/" element={<Header cart={cart}></Header>}>
-            <Route index element={<Home></Home>}></Route>
-            <Route path="/about" element={<About></About>}></Route>
-            <Route
-              path="/products"
-              element={<Product cart={cart} setCart={setCart}></Product>}
-            ></Route>
-            <Route
-              path="/cart"
-              element={<Cart cart={cart} setCart={setCart}></Cart>}
-            ></Route>
-          </Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/products"
+            element={<Product cart={cart} setCart={setCart} />}
+          />
+          <Route
+            path="/cart"
+            element={<Cart cart={cart} setCart={setCart} />}
+          />
         </Routes>
       </BrowserRouter>
       <Footer />
